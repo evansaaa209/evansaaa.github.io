bc6a02812a09945cace66c7561b01e3b3a8387cc
evansaaa209 <42775234+evansaaa209@users.noreply.github.com>
2019-02-01 07:47:45 -0500

    Add files via upload

---
+++ 01/script.js
@@ -0,0 +1,9 @@
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%"; 
+  }
+}
+
+function ani(){
+  document.getElementsByClassName('rotate-center').rotate-center;
+}
